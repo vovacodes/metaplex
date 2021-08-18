a29ba6cffe57438eef85a7bbd90de348334bdb22
Vladimir Guguiev <wizardzloy@gmail.com>
2021-08-19 00:52:29 +0300

    Updates

---
+++ _next/static/wlitCx6CK112bywm-BUMA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-17f6d924639d7d38054e.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
